1bbd4b89098f7af6b666e42b13dda69dfff6b7dd
Alex Sirota <67526318+ajs256@users.noreply.github.com>
2023-11-05 20:48:48 -0800

    init

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class DriveCommand extends CommandBase {
+
+    private final SwerveSubsystem subsys;
+
+    private final DoubleSupplier vX, vY;
+    private final DoubleSupplier rX, rY;
+
+    public DriveCommand(SwerveSubsystem subsys, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier rX, DoubleSupplier rY) {
+        this.subsys = subsys;
+
+        this.vX = vX;
+        this.vY = vY;
+
+        this.rX = rX;
+        this.rY = rY;
+
+        addRequirements(this.subsys);
+    }
+
+    @Override
+    public void execute() {
+        // Read from joysticks
+        double driveX = vX.getAsDouble();
+        double driveY = vY.getAsDouble();
+        double rotX = rX.getAsDouble();
+        double rotY = rY.getAsDouble();
+
+        ChassisSpeeds speeds = subsys.getTargetSpeeds(driveX, driveY, rotX, rotY);
+        // subsys.drive(speeds);
+
+        subsys.driveFieldOriented(speeds);
+
+    }
+    
+}
